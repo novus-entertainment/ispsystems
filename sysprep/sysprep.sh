9c326103f3474582437fc977b9b8008aa2302abe
brianhillnovus <113715294+brianhillnovus@users.noreply.github.com>
2022-09-16 11:57:10 -0700

    Update sysprep.sh

--- sysprep/sysprep.sh
+++ sysprep/sysprep.sh
@@ -1,1 +1,22 @@
+#!/bin/bash
 
+############################################################################
+# Ubuntu VM Sysprep Script
+#
+# Created by: Brian Hill
+# Version 0.1 - September 1, 2022
+#
+# Run this script to configure the newly deployed VM.
+#    - Check for script update and restart script if found
+#    - Set hostname
+#    - Set timezone
+#    - Configuire network interfaces
+#    - Configure stock firewall rules
+#    - Update system
+#    - Install common utilities
+#    - Join Active Directory
+#    - Install Zabbix Agent 2 (Active)
+############################################################################
+
+# Script version. Used for auto-updating from git repository.
+ver=0.1
