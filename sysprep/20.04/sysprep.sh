604ce64659f23729b340657ad100ea566fad9aa3
brianhillnovus <113715294+brianhillnovus@users.noreply.github.com>
2022-12-06 11:08:01 -0800

    Update sysprep.sh

--- sysprep/20.04/sysprep.sh
+++ sysprep/20.04/sysprep.sh
@@ -4,7 +4,7 @@
 # Ubuntu 20.04 Sysprep Script
 #
 # Created by: Brian Hill
-# Version 0.15 - November 22, 2022
+# Version 0.16 - December 6, 2022
 #
 # Run this script to configure the newly deployed VM.
 #    - Check for script update and restart script if found
@@ -19,7 +19,7 @@
 ###################################################################################################
 
 # Script version. Used for auto-updating from git repository.
-ver="0.15"
+ver="0.16"
 
 # Reset all screen formatting and clear screen
 printf "\033[0m"
