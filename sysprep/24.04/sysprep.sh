0556f12caa7054d51c5bcfa060dc4826200ab8dd
brianhillnovus <113715294+brianhillnovus@users.noreply.github.com>
2024-07-30 11:11:29 -0700

    Update sysprep.sh

--- sysprep/24.04/sysprep.sh
+++ sysprep/24.04/sysprep.sh
@@ -20,7 +20,7 @@
 ###################################################################################################
 
 # Script version. Used for auto-updating from git repository.
-ver=1
+ver=2
 
 # Reset all screen formatting and clear screen
 printf "\033[0m"
